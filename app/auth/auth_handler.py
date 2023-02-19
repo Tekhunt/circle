3a5667eff86282d5f6442e7674d545f6a0774cd4
tekhunt <chizzydmec@gmail.com>
2023-02-19 22:42:12 +0100

    Remove comments

--- app/auth/auth_handler.py
+++ app/auth/auth_handler.py
@@ -1,4 +1,3 @@
-# This file is responsible for signing , encoding , decoding and returning JWTS
 import time
 from typing import Dict
 
@@ -14,7 +13,6 @@ def token_response(token: str):
     return {"access_token": token}
 
 
-# function used for signing the JWT string
 def signJWT(user_id: str) -> Dict[str, str]:
     payload = {"user_id": user_id, "expires": time.time() + 600}
     token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
